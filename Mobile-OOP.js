6e4275320ca6ff7706165b801cb7b25095d768e6
Kingsolomon <Inboxlegacy@gmail.com>
2017-04-10 16:42:21 +0100

    created OOP showing with Mobile phone properties

---
+++ Mobile-OOP.js
@@ -0,0 +1,55 @@
+class Mobile {
+	constructor(IEMICode, SIMcard, Processor, InternalMemory, IsSingleSIM){
+		this.IEMICode = IEMICode;
+		this.SIMcard = SIMcard;
+		this.Processor = Processor;
+		this.InternalMemory = InternalMemory;
+		this.IsSingleSIM = IsSingleSIM;
+	}
+
+
+	GetIEMICode(IEMICode){
+		this.IEMICode = IEMICode;
+		return "IEMI Code -" + IEMICode;
+	}  
+  
+    Dial(){
+    	return "Dial a number";
+    }
+
+    Receive(){
+    	return "Receive a call";
+    }
+
+    SendMessage(){
+    	return "Message Sent";  
+    }
+}
+
+
+
+class Samsumg extends Mobile{  
+    GetWIFIConnection(){  
+        return "WIFI connected";  
+    }  
+  
+    //This is one overloaded method which shows camera functionality as well but with its camera's different mode(panaroma)  
+    CameraClick(CameraMode)  
+    {  
+        return "Camera clicked in " + CameraMode + " Mode";  
+    }  
+}
+
+
+
+class Nokia extends Mobile{
+    GetBlueToothConnection(){  
+        return "Bluetooth connected";  
+    }  
+   
+    //New implementation for this method which was available in Mobile Class  
+    //This is runtime polymorphism  
+    SendMessage(){  
+        return "Message Sent to a group";  
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Andela-Assessment-2
 For Andela Day 2
+
+This is a Github repo containing a real-world problem modeled using OOP while taking advantage of inheritance, encapsulation, polymorphism and the other OOP concepts.
+
+This problem is about Mobile phones. Nokia and Samsung was considered here.
